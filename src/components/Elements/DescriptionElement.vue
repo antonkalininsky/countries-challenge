d772875cff0bf1afdbaba01b863249ad1cff98bb
Anton <chadik.free@gmail.com>
2023-05-03 21:59:29 +0600

    Верстка экрана CountryPage

---
+++ src/components/Elements/DescriptionElement.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'DescriptionElement',
+    data() {
+        return {
+            items: 8
+        }
+    }
+};
+</script>
+
+<template lang="">
+    <b-container>
+        <b-row>
+            <b-col cols="12" class="pb-3">
+                <h2 class="text-w-800">Country</h2>
+            </b-col>
+            <b-col cols="12" class="pb-5">
+                <b-container>
+                    <b-row>
+                        <b-col cols="6" v-for="item in items" :key="item" class="text-small px-0 pb-1">
+                            <span class="text-w-800">header: </span> data
+                        </b-col>
+                    </b-row>
+                </b-container>
+
+            </b-col>
+            <b-col cols="12">
+                buttons
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<style lang=""></style>
--- src/components/Pages/CountryPage.vue
+++ src/components/Pages/CountryPage.vue
@@ -1,13 +1,33 @@
 <script>
+import DescriptionElement from "../Elements/DescriptionElement.vue";
+
 export default {
     name: "CountryPage",
+    components: {
+        DescriptionElement,
+    },
 };
 </script>
 
 <template lang="">
-    <div>Hi I am CountryPage
-        {{ $store.state.count }}
-    </div>
+    <b-container>
+        <b-row>
+            <b-col class="py-5">
+                <b-button
+                    variant="black,link"
+                    class="px-4 shadow-sm text-small"
+                    to="/"
+                >
+                    <i class="fa-solid fa-arrow-left pr-2"></i>
+                    Back
+                </b-button>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols="5">Флаг</b-col>
+            <b-col><DescriptionElement /></b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <style scoped></style>
